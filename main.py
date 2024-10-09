558dd750c7f9e1c312df426bb3046d3237f95297
Daffabot <daffabot@programmer.net>
2024-10-09 02:36:44 +0000

    Update lengkap 1

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from flask import Flask, request, render_template
 app=Flask(__name__)
 
+# main index
+@app.route('/')
+def index():
+    return render_template('index.html',title="")
+
+# admin login
 @app.route('/admin/')
 def adminIndex():
-    return render_template('admin/index.html',
-    title="Admin Login")
+    return render_template('admin/index.html',title="Admin Login")
 
+# ---------------------------user area----------------------------------
+
+# user login
+@app.route('/user/')
+def userIndex():
+    return render_template('user/index.html',title="User Login")
 
 if __name__=="__main__":
     app.run(debug=True)
